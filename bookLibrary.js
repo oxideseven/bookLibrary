896f4461a8c61a96b4c525aefdceb6928fa0d29c
Aemilia <oxideseven@users.noreply.github.com>
2022-01-24 10:55:50 -0600

    Add: further building of basic functionality

--- bookLibrary.js
+++ bookLibrary.js
@@ -1,3 +1,25 @@
+let library = [];
+
+//DOM elements
+const newBookBtn = document.getElementById('newBookBtn');
+const addBookBtn = document.getElementById('addBookBtn');
+const clrFormBtn = document.getElementById('clrFormBtn');
+const closeFormBtn = document.getElementById('closeFormBtn');
+const inputs = document.querySelectorAll('input');
+const cards = document.getElementById('cards');
+
+addBookBtn.addEventListener('click', addToLibrary);
+newBookBtn.addEventListener('click', openForm);
+closeFormBtn.addEventListener('click', closeForm);
+clrFormBtn.addEventListener('click', clearForm);
+
+//Form Fields
+const bkTitle = document.getElementById('bkTitle');
+const bkAuthor = document.getElementById('bkAuthor');
+const bkPages = document.getElementById('bkPages');
+const bkRead = document.getElementById('bkRead');
+
+//Constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -5,15 +27,60 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+//to call info on a specific book
 Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, read: ${this.read}`;
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+//Add a book to the library
+function addToLibrary() {
+    library.push(new Book(bkTitle.value, bkAuthor.value, bkPages.value, bkRead.checked));
+    displayBooks();
+}
 
-const cavesOfSteel = Object.create(Book.prototype);
-cavesOfSteel.title = 'Caves of Steel';
-cavesOfSteel.author = 'Isaac Asimov';
-cavesOfSteel.pages = 224;
-cavesOfSteel.read = true;
+//Form functions
+
+function openForm() {
+    document.getElementById("popupForm").style.display = "flex";
+}
+
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+}
+
+window.onclick = function (event) {
+    let modal = document.getElementById('popupForm');
+    if (event.target == modal) {
+      closeForm();
+    }
+}
+
+function displayBooks() {
+    cards.innerHTML = "";
+    library.forEach((result, idx) => {
+        const card = document.createElement('div');
+        card.classlist = 'card-body';
+
+        const content =  `
+        <div class="card" id="card${idx}">
+          <div class="card-body">
+            <h5>${result.title}</h5>
+            <p>by: ${result.author}</p>
+            <p>${result.pages} pages</p>
+            <p>Read: ${result.read}</p>
+          </div>
+      </div>
+      `;
+    
+      // Append newyly created card element to the container
+      cards.innerHTML += content;
+    })
+}
+
+function clearForm() {
+    inputs.forEach(input => input.value = '');
+}
+
+closeForm();
+displayBooks();
 
